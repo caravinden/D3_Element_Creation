e0b6fe9994a3725a2d470ffbee93a6c6604c8886
aravind <aravindcheekkallur@am.amrita.edu>
2015-01-05 15:56:14 +0530

    circle creation using d3.js

---
+++ js/d3_element.js
@@ -0,0 +1,45 @@
+/**
+ * Java script library on top of d3. 
+ */
+var d3Elements = (function(){
+	var lib = function(){
+		
+	},
+	data = function(){
+		
+	},
+	element = function(){
+		
+	}
+	return {
+		version : '0.1',
+		lib : lib,
+		element : element,
+		data : data
+	}
+})();
+
+/**
+ * To stringify data
+ * @param data
+ * @returns
+ */
+d3Elements.data.stringify = function(data){
+	return JSON.stringify(data);
+}
+
+/**
+ * Java script function to create circle using d3
+ * @param divId
+ * @param data
+ * @param cirRadius
+ */
+d3Elements.element.circle = function(divId,cirRadius){
+
+	  d3.select("#"+divId).append("svg")
+	    .append("circle")
+	    .attr("cx",100)
+	    .attr("cy",100)
+	    .attr("r",cirRadius)
+	    .style("fill", "green");
+}
